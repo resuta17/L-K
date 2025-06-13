767eaa6ea237a2124522d4d50b79c437cc6eed4d
Lester <jlhebres15@gmail.com>
2025-06-13 18:05:28 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="rounded-b-lg h-98 opacity-80 bg-[url('https://scontent.flgp1-1.fna.fbcdn.net/v/t39.30808-6/474745175_1313407919683538_4754889622248912206_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHD8dq95sD840pY5U0QChqpo0OjjEPcQYKjQ6OMQ9xBguS-HqlBAFu3pdyiakSVVxgEX5oXcfeFEQqHjcIsOevk&_nc_ohc=pkNpWIBG9xwQ7kNvwFF1rNN&_nc_oc=AdmEXw3JBslomObEj8ymuAs-D6vdYFl_3nrQNd8yVyrEuJN0AxiKtnkd0_lRFSCGR2s&_nc_zt=23&_nc_ht=scontent.flgp1-1.fna&_nc_gid=09DmKSIoLvxt1S3j1_4mRg&oh=00_AfPZobIf77NLdVrq2-vvsH7Jrova0nSu5SjLXMy-3oODKw&oe=6851B1FF')] bg-cover bg-center" >
+     <header class="max-width bg-yellow-600	h-24 flex items-center justify-center sticky rounded-br-lg opacity-80">
+        <img :src="'https://cdn-icons-png.flaticon.com/128/10461/10461387.png'" class="ml-20 w-14"/>
+        <p class="text-white font-bold text-3xl m-2">
+          LK's Minimart
+        </p>  
+     </header>
+  </div>
+  <div class="flex items-center justify-center space-x-2 mt-4">
+      <div>
+        <img :src="'https://cdn-icons-png.flaticon.com/128/684/684908.png'" class="w-6 h-6"/>
+      </div>
+      <div>
+        <a target="_blank" href="https://www.google.com/maps/place/Leanne+and+Kirby+Balloons+Party/@12.8451868,123.990664,20.11z/data=!4m6!3m5!1s0x33a0c2c4190c3415:0xdc997e7a68e532fc!8m2!3d12.8450916!4d123.9905773!16s%2Fg%2F11f15pz34y?entry=ttu&g_ep=EgoyMDI1MDYxMC4xIKXMDSoASAFQAw%3D%3D">
+           Purok 4, South Poblacion, Juban, Sorsogon
+        </a>      
+      </div>
+  </div>
+
+ 
+  
+  <body class="m-10 mt-18">
+    <section class=" flex items-start justify-center mt-10">
+      <div class="flex-col">
+        <p class="font-bold text-2xl text-center mb-2">
+          STORE PROFILE
+        </p>
+        <p>
+          LK’s Minimart is your friendly neighborhood one-stop shop for everyday essentials and celebration needs. 
+        </p>   
+      </div>
+      
+    </section>
+  </body>
+  
+</template>
+
+<style scoped>
+
+  
+</style>
